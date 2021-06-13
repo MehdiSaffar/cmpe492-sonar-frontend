df73dae22c6db8950c2fa2aecb75a60e662294ed
adilnumancelik <adil@shopihq.com>
2021-06-13 23:53:34 +0300

    add sonar auth0 api identity

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ ReactDOM.render(
             domain={process.env.REACT_APP_AUTH0_DOMAIN}
             clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
             redirectUri={window.location.origin}
+            audience="https://sonar-authorize/"
         >
             <Router>
                 <AppContextProvider>
