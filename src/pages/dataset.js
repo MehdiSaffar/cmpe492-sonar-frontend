f910d166c870a87e58e0ee64494d08e336fefe6f
Mehdi Saffar <saffar.mehdi3@gmail.com>
2021-06-25 14:18:42 +0300

    modifs to dataset

--- src/pages/dataset.js
+++ src/pages/dataset.js
@@ -1,6 +1,7 @@
 import './dataset.less'
 
 import { Button, PageHeader, Table } from 'antd'
+import { useMemo } from 'react'
 
 import StatusTag from '../comp/StatusTag'
 import { useApi, useRouter } from '../hooks'
@@ -15,12 +16,12 @@ const columns = [
             return <StatusTag type={'article'} status={status} />
         }
     },
-    {
-        title: 'ID',
-        dataIndex: 'id',
-        key: 'id',
-        render: returnOr
-    },
+    // {
+    //     title: 'ID',
+    //     dataIndex: 'id',
+    //     key: 'id',
+    //     render: returnOr
+    // },
     {
         title: 'DOI',
         dataIndex: 'doi',
@@ -34,7 +35,7 @@ const columns = [
         render: returnOr
     },
     {
-        title: 'Created at',
+        title: 'Published at',
         dataIndex: 'created_date',
         key: 'created_date',
         render: renderDateToNow
@@ -50,13 +51,13 @@ const columns = [
         dataIndex: 'processed_date',
         key: 'processed_date',
         render: renderDateToNow
-    },
-    {
-        title: 'Tries',
-        dataIndex: 'try_count',
-        key: 'try_count',
-        render: returnOr
     }
+    // {
+    //     title: 'Tries',
+    //     dataIndex: 'try_count',
+    //     key: 'try_count',
+    //     render: returnOr
+    // }
 ]
 
 export default function Dataset(props) {
@@ -66,6 +67,24 @@ export default function Dataset(props) {
     const [dataset, loading, refresh] = useApi('get', `/article-list/${id}`, {
         processData: formatArticleListResponse
     })
+
+    const [fetchCount, processingCount, doneCount] = useMemo(() => {
+        if (!dataset) {
+            return []
+        }
+
+        let fetchCount = 0
+        let processingCount = 0
+        let doneCount = 0
+
+        for (let article of dataset) {
+            fetchCount += article.status == 'to_be_fetched'
+            processingCount += article.status == 'to_be_processed'
+            doneCount += article.status == 'done'
+        }
+
+        return [fetchCount, processingCount, doneCount]
+    }, [dataset])
 
     console.log(dataset)
 
@@ -77,13 +96,24 @@ export default function Dataset(props) {
         <div>
             <PageHeader
                 title={`Article list ${dataset?.title ?? ''}`}
-                subTitle="Here you can find the articles of this list"
+                subTitle={
+                    <div>
+                        Here you can find the articles of this list
+                        <br />
+                        Fetching: {fetchCount}, Processing: {processingCount}, Done: {doneCount}
+                    </div>
+                }
                 onBack={() => router.history.push('/dashboard')}
                 extra={[
                     <Button key="2" onClick={refresh}>
                         Refresh
                     </Button>,
-                    <Button key="1" type="primary" onClick={onClickVisualize}>
+                    <Button
+                        disabled={fetchCount !== 0 && processingCount !== 0}
+                        key="1"
+                        type="primary"
+                        onClick={onClickVisualize}
+                    >
                         Visualize
                     </Button>
                 ]}
